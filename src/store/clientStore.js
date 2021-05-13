72bca2c5e6afe1d60c3636c635324fd94f0ee77c
Nagy Ervin <nagyervin.bws@gmail.com>
2021-05-13 23:18:42 +0300

    initialize store

---
+++ src/store/clientStore.js
@@ -0,0 +1,30 @@
+const clientStore = {
+    namespaced: true,
+    state: () => {
+        return {
+            clients: [],
+            sortFields:[
+                {
+                    name : 'name',
+                    direction: null
+                }
+            ],
+        }
+    },
+    mutations: {
+        setClients: (state, value) => state.clients = value,
+        setSortField: (state, value) => {
+            state.sortFields = state.sortFields.map(f => f.name === value.name ? value : f);
+        },
+        clearSortFields: (state) => state.sortFields = state.sortFields.map(f => {
+            return {...f, direction: null}
+        }),
+    },
+    actions: {
+        loadItems: ({commit}) => {
+            // TODO: implement Ajax request (install axios)
+        }
+    }
+}
+
+export default clientStore;
